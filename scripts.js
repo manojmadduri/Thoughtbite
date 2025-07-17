2382d690b30dbd474df7bc1a3594cc04b0fe9c4e
Manoj <121824103+manojmadduri@users.noreply.github.com>
2025-07-16 23:09:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
       title: "Hidden VS Code Tool",
       link: "video-hidden-vscode-tool.html"
     }
+    {
+    title: "AI Critical Thinking Prompt",
+    link: "ai-prompt.html"
+  }
   ];
 
   const container = document.getElementById("pastVideos");
